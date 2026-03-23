8837e8280b86a8ebe17965f993b3f1e116f07190
VanshShah2004 <shahvanshm23.4.2004@gmail.com>
2026-03-23 20:21:55 +0530

    React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
-import ToDoList from './ToDoList.jsx'
-
+import ColorPicker from './ColorPicker.jsx'
 
 function App() {
   return (
-    <ToDoList />
+    
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<h1>Hi</h1>} />
+      <Route path='/login' element = {<ColorPicker/> }/>
+    </Routes>
+    </BrowserRouter>
+   
+    
   )
 }
 export default App;
